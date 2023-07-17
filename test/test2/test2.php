b6eb016908aef09773cf2204402abdd96065bded
NTT1906 <nttislinked@gmail.com>
2023-07-17 15:06:41 +0700

    Test SQL 2

--- test/test2/test2.php
+++ test/test2/test2.php
@@ -1,29 +1,32 @@
 <?php
-/**
- * This file is part of PROJECT_NAME
- *
- * @author Arisify
- * @link   https://github.com/Arisify
- * @license https://opensource.org/licenses/MIT MIT License
- *
- * •.,¸,.•*`•.,¸¸,.•*¯ ╭━━━━━━━━━━━━╮
- * •.,¸,.•*¯`•.,¸,.•*¯.|:::::::/\___/\
- * •.,¸,.•*¯`•.,¸,.•* <|:::::::(｡ ●ω●｡)
- * •.,¸,.•¯•.,¸,.•╰ *  し------し---Ｊ
- *
- */
-declare(strict_types=1);
-
-
-class MyDB extends SQLite3{
-    function __construct(){
+class MyDB extends SQLite3
+{
+    function __construct()
+    {
         $this->open('test.db');
     }
 }
-
 $db = new MyDB();
-if (!$db) {
+if(!$db){
     echo $db->lastErrorMsg();
 } else {
     echo "Opened database successfully\n";
 }
+
+$sql =<<<EOF
+      CREATE TABLE COMPANY
+      (ID INT PRIMARY KEY     NOT NULL,
+      NAME           TEXT    NOT NULL,
+      AGE            INT     NOT NULL,
+      ADDRESS        CHAR(50),
+      SALARY         REAL);
+EOF;
+
+$ret = $db->exec($sql);
+if(!$ret){
+    echo $db->lastErrorMsg();
+} else {
+    echo "Table created successfully\n";
+}
+$db->close();
+?>
