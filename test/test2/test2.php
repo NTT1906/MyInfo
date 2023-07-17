303495fdf4ac6f29e0b3530bcb2a7c3e895bdd5a
NTT1906 <nttislinked@gmail.com>
2023-07-17 15:07:34 +0700

    Test SQL 4

--- test/test2/test2.php
+++ test/test2/test2.php
@@ -14,24 +14,16 @@ if(!$db){
 }
 
 $sql =<<<EOF
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (1, 'Paul', 32, 'California', 20000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (2, 'Allen', 25, 'Texas', 15000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (3, 'Teddy', 23, 'Norway', 20000.00 );
-
-      INSERT INTO COMPANY (ID,NAME,AGE,ADDRESS,SALARY)
-      VALUES (4, 'Mark', 25, 'Rich-Mond ', 65000.00 );
+      SELECT * from COMPANY;
 EOF;
 
-$ret = $db->exec($sql);
-if(!$ret){
-    echo $db->lastErrorMsg();
-} else {
-    echo "Records created successfully\n";
+$ret = $db->query($sql);
+while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
+    echo "ID = ". $row['ID'] . "\n";
+    echo "NAME = ". $row['NAME'] ."\n";
+    echo "ADDRESS = ". $row['ADDRESS'] ."\n";
+    echo "SALARY =  ".$row['SALARY'] ."\n\n";
 }
+echo "Operation done successfully\n";
 $db->close();
 ?>
